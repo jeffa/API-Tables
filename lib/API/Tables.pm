34fa6400a168b60410fe7272ef781a61032edc28
Jeff Anderson <jeffa@unlocalhost.com>
2024-01-27 18:02:05 -0600

    trying to build simple API

--- lib/API/Tables.pm
+++ lib/API/Tables.pm
@@ -1,10 +1,16 @@
 package API::Tables;
 use Dancer ':syntax';
+use Spreadsheet::HTML;
 
 our $VERSION = '0.1';
+our $generator = Spreadsheet::HTML->new( indent => '  ' );
 
-get '/' => sub {
-    template 'index';
+get '/*' => sub {
+    my ($style) = splat;
+    my %valid = map {$_=>1} qw(generate landscape portrait);
+    $style = 'generate' unless $valid{$style};
+    content_type 'text/plain';
+    return $generator->$style([0..3],[4..7],[8..11]);
 };
 
 true;
