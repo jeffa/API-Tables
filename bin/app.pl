c38407607aec5e53da064d58c4d6a7c81e95dea5
Jeff Anderson <jeffa@unlocalhost.com>
2024-01-30 06:37:18 -0600

    ready to go

--- bin/app.pl
+++ bin/app.pl
@@ -1,4 +1,9 @@
 #!/usr/bin/env perl
 use Dancer;
 use API::Tables;
+use File::Pid;
+ 
+my $pidfile = File::Pid->new({ file => '/home/jeffa/api.pid' });
+$pidfile->write;
+
 dance;
--- lib/API/Tables.pm
+++ lib/API/Tables.pm
@@ -12,13 +12,17 @@ our $generator = Spreadsheet::HTML->new();
 get '/conway' => sub {
     my @valid = qw(wechsler pad size on off colors fade interval alpha block jquery);
     my @params = map { defined(params->{$_}) ? ( $_ => params->{$_} ) : () } @valid;
+    #header('Access-Control-Allow-Origin' => '*');
     return { board => $generator->conway( @params ) };
 };
 
 get '/sudoku' => sub {
+    my @valid = qw(blanks attempts jquery);
+    my @params = map { defined(params->{$_}) ? ( $_ => params->{$_} ) : () } @valid;
     my $blanks   = params->{blanks}   || 50;
     my $attempts = params->{attempts} || 3;
-    return { board => $generator->sudoku( blanks => $blanks, attempts => $attempts ) };
+    #header('Access-Control-Allow-Origin' => '*');
+    return { board => $generator->sudoku( @params ) };
 };
 
 # curl -d '{"data":[["foo","bar","baz"],[1,2,3]]}' -H "Content-Type: application/json" -X POST http://www.unlocalhost.com:3000/landscape
@@ -26,6 +30,7 @@ post '/*' => sub {
     my ($style) = splat;
     my %valid = map {$_=>1} qw(generate landscape portrait);
     $style = 'generate' unless $valid{$style};
+    #header('Access-Control-Allow-Origin' => '*');
     return { spreadsheet => $generator->$style( data => params->{data} ) };
 };
 
