40914f5066389dc6455ffbca70b3db9add62800d
Jeff Anderson <jeffa@unlocalhost.com>
2024-01-30 11:14:06 -0600

    added beadwork and before hook

--- lib/API/Tables.pm
+++ lib/API/Tables.pm
@@ -8,30 +8,36 @@ use JSON;
 our $VERSION = '0.1';
 our $generator = Spreadsheet::HTML->new();
 
+hook 'before' => sub {
+    header('Access-Control-Allow-Origin' => 'https://www.unlocalhost.com');
+};
+
+get '/beadwork' => sub {
+    my @valid = qw( preset art map bgcolor jquery scroll by bx bgdirection );
+    my @params = map { defined(params->{$_}) ? ( $_ => params->{$_} ) : () } @valid;
+    return { board => $generator->beadwork( @params ) };
+};
 
 get '/conway' => sub {
-    my @valid = qw(wechsler pad size on off colors fade interval alpha block jquery);
+    my @valid = qw( wechsler pad size on off colors fade interval alpha block jquery );
     my @params = map { defined(params->{$_}) ? ( $_ => params->{$_} ) : () } @valid;
-    header('Access-Control-Allow-Origin' => 'https://www.unlocalhost.com');
     return { board => $generator->conway( @params ) };
 };
 
 get '/sudoku' => sub {
-    my @valid = qw(blanks attempts jquery);
+    my @valid = qw( blanks attempts jquery );
     my @params = map { defined(params->{$_}) ? ( $_ => params->{$_} ) : () } @valid;
-    my $blanks   = params->{blanks}   || 50;
-    my $attempts = params->{attempts} || 3;
-    header('Access-Control-Allow-Origin' => 'https://www.unlocalhost.com');
     return { board => $generator->sudoku( @params ) };
 };
 
-# curl -d '{"data":[["foo","bar","baz"],[1,2,3]]}' -H "Content-Type: application/json" -X POST http://www.unlocalhost.com:3000/landscape
-post '/*' => sub {
+# curl -d '{"data":[["foo","bar","baz"],[1,2,3]]}' -H "Content-Type: application/json" -X POST http://jeffa.unlocalhost.com/landscape
+any ['get', 'post'] => '/*' => sub {
     my ($style) = splat;
-    my %valid = map {$_=>1} qw(generate landscape portrait);
+    my %valid = map {$_=>1} qw( generate landscape portrait );
     $style = 'generate' unless $valid{$style};
-    header('Access-Control-Allow-Origin' => 'https://www.unlocalhost.com');
-    return { spreadsheet => $generator->$style( data => params->{data} ) };
+    my @valid = qw( data );
+    my @params = map { defined(params->{$_}) ? ( $_ => params->{$_} ) : () } @valid;
+    return { spreadsheet => $generator->$style( @params ) };
 };
 
 true;
